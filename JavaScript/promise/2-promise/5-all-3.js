60ae3a0b68eb158e01e7e2bf3a4427d3c1a2799d
lakiliu <lakiliu@tencent.com>
2022-07-29 16:07:34 +0800

    finally

--- JavaScript/promise/2-promise/5-all-3.js
+++ JavaScript/promise/2-promise/5-all-3.js
@@ -1,11 +1,6 @@
 /**
  * @description:Promise.all
  * @author: lakiliu
- * all 实现的功能
- * 1. 传入的数组 支持同步和异步，支持按顺序返回
- * 2. 所有的状态fulfilled, all 的状态才是fulfilled
- * 3. 如果之中有一个rejected,则第一个rejected的实例被返回。 直接触发rejected
- * 
  */
 
 let Promise1 = require('./Promise')
@@ -16,33 +11,32 @@ function read(url) {
   fs.readFile(url, 'utf8', function (error, data) {
     if (error) dfd.reject(error)
     dfd.resolve(data)
+
   })
   return dfd.promise
 }
-const isPromise = (value) => {
-  if (typeof value === 'object' && value !== null || typeof value === 'function') {
-    if (typeof value.then === 'function') {
+function isPromise(obj) {
+  if (typeof obj === 'object' && obj !== null || typeof obj === 'function') {
+    if (typeof obj.then === 'function') {
       return true;
     } else {
       return false;
     }
-
   }
 }
+
 Promise.all = function (values) {
   return new Promise((resolve, reject) => {
-    let arr = [];
+    let res = [];
     let index = 0;
-
     function processData(key, value) {
-      arr[key] = value;
+      res[key] = value;
       if (++index === values.length) {
-        resolve(arr)
+        resolve(res)
       }
     }
-
     for (let i = 0; i < values.length; i++) {
-      let current = values[i];
+      let current = values[i]
       if (isPromise(current)) {
         current.then(data => {
           processData(i, data)
@@ -53,6 +47,9 @@ Promise.all = function (values) {
     }
   })
 }
+
 Promise.all([1, 2, 3, read('./name.txt'), 5, 6, 7]).then(data => {
-  console.log(data)
+  console.log(111, data)
+}, error => {
+  console.log(222, error)
 })
