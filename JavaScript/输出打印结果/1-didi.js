4689098d04d06b46325345a299b6e6b1cf36aa3d
lakiliu <lakiliu@tencent.com>
2022-08-21 15:02:17 +0800

    1

---
+++ JavaScript/输出打印结果/1-didi.js
@@ -0,0 +1,27 @@
+function Foo() {
+  getName = function () {
+    console.log(1);
+  };
+  return this;
+}
+Foo.getName = function () {
+  console.log(2);
+};
+Foo.prototype.getName = function () {
+  console.log(3);
+};
+var getName = function () {
+  console.log(4);
+};
+function getName() {
+  console.log(5);
+}
+
+//请写出以下输出结果：
+Foo.getName(); // 2
+getName(); // 5
+Foo().getName(); // 5
+getName(); // 5
+new Foo.getName(); // 2
+new Foo().getName(); // 1
+new new Foo().getName(); //1
