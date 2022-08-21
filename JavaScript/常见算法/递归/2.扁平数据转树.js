ae677e926bf370c7173dbf894f6740333f5f35bd
lakiliu <lakiliu@tencent.com>
2022-08-21 17:17:38 +0800

    11

---
+++ JavaScript/常见算法/递归/2.扁平数据转树.js
@@ -0,0 +1,71 @@
+// 输入结果
+let arr = [
+  { id: 1, name: '部门1', pid: 0 },
+  { id: 2, name: '部门2', pid: 1 },
+  { id: 3, name: '部门3', pid: 1 },
+  { id: 4, name: '部门4', pid: 3 },
+  { id: 5, name: '部门5', pid: 4 },
+]
+// 输出结果
+
+let result = [
+  {
+    "id": 1,
+    "name": "部门1",
+    "pid": 0,
+    "children": [
+      {
+        "id": 2,
+        "name": "部门2",
+        "pid": 1,
+        "children": []
+      },
+      {
+        "id": 3,
+        "name": "部门3",
+        "pid": 1,
+        "children": [
+          // 结果 ,,,
+        ]
+      }
+    ]
+  }
+]
+
+//主要思路也是先把数据转成Map去存储，之后遍历的同时借助对象的引用，直接从Map找对应的数据做存储。不同点在遍历的时候即做Map存储,有找对应关系。性能会更好。
+
+
+function arrayToTree(items) {
+  const result = [];   // 存放结果集
+  const itemMap = {};  // 
+  for (const item of items) {
+    const id = item.id;
+    const pid = item.pid;
+
+    if (!itemMap[id]) {
+      itemMap[id] = {
+        children: [],
+      }
+    }
+
+    itemMap[id] = {
+      ...item,
+      children: itemMap[id]['children']
+    }
+
+    const treeItem = itemMap[id];
+
+    if (pid === 0) {
+      result.push(treeItem);
+    } else {
+      if (!itemMap[pid]) {
+        itemMap[pid] = {
+          children: [],
+        }
+      }
+      itemMap[pid].children.push(treeItem)
+    }
+
+  }
+  return result;
+}
